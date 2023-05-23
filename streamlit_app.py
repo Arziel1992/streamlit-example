e2d68e8d6e65af99c32633cac55ad3dff4d4bf6f
Arziel1992 <nico.ketterer@live.cl>
2023-05-23 14:33:35 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,35 +17,38 @@ df1, df2, df3, df4 = load_data()
 # Create network graph
 def create_network_graph(df):
     G = nx.Graph()
+    root_values = []
     for i in range(len(df)):
         root = df.columns[0] if 'id' not in df.columns else 'id' # Use the first column if 'id' does not exist
         root_value = df.iloc[i][root]
+        root_values.append(root_value)
         G.add_node(root_value)
         for col in df.columns:
             if col != root:
                 G.add_node(df.iloc[i][col])
                 G.add_edge(root_value, df.iloc[i][col])
-    return G
+    return G, root_values
 
 # Draw network graph
-def draw_network_graph(G):
+def draw_network_graph(G, root_values):
     fig, ax = plt.subplots(figsize=(8,6))
-    nx.draw(G, with_labels=True, ax=ax)
+    node_colors = ["green" if node in root_values else "red" for node in G.nodes()]
+    nx.draw(G, with_labels=True, node_color=node_colors, ax=ax)
     st.pyplot(fig)
 
 # Create and draw network graph for each dataframe
 st.header('Women on Remand')
-G1 = create_network_graph(df1)
-draw_network_graph(G1)
+G1, root_values1 = create_network_graph(df1)
+draw_network_graph(G1, root_values1)
 
 st.header('Women Received into Prison')
-G2 = create_network_graph(df2)
-draw_network_graph(G2)
+G2, root_values2 = create_network_graph(df2)
+draw_network_graph(G2, root_values2)
 
 st.header('Women on Supervised Orders (Current)')
-G3 = create_network_graph(df3)
-draw_network_graph(G3)
+G3, root_values3 = create_network_graph(df3)
+draw_network_graph(G3, root_values3)
 
 st.header('Women on Supervised Orders (Starting)')
-G4 = create_network_graph(df4)
-draw_network_graph(G4)
+G4, root_values4 = create_network_graph(df4)
+draw_network_graph(G4, root_values4)
