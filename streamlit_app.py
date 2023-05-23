54e7759c05171fcad83f59f2050176ed3558e3c9
Arziel1992 <nico.ketterer@live.cl>
2023-05-23 13:43:23 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import networkx as nx
 from pyvis.network import Network
 
 # Load data
-@st.cache
+@st.cache_data  # Updated decorator
 def load_data():
     data = pd.read_csv('your_data.csv')
     return data
@@ -12,13 +12,12 @@ def load_data():
 df = load_data()
 
 # Initialize the network
-net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white", directed=True)
+net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
 
 # add nodes for 'person'
 persons = df['RecordID'].unique()
 for person in persons:
-    person_str = str(person)
-    net.add_node(person_str, label=person_str, color='green')
+    net.add_node(person, label=person, color='green')
 
 # add nodes for other categories and connect them with 'person'
 categories = df.columns.to_list()
@@ -27,16 +26,11 @@ categories.remove('RecordID')
 for category in categories:
     unique_values = df[category].unique()
     for value in unique_values:
-        value_str = str(value)
-        net.add_node(f'{category}_{value_str}', label=value_str, color='red')
+        net.add_node(f'{category}_{value}', label=value, color='red')
         subset = df[df[category] == value]
         for i in subset['RecordID']:
-            net.add_edge(str(i), f'{category}_{value_str}')
+            net.add_edge(i, f'{category}_{value}')
             
-# Save the graph to an HTML file
-net.save_graph("network.html")
-
-# Display the graph
-HtmlFile = open("network.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-st.markdown(source_code, unsafe_allow_html=True)
+# display the graph
+net.show('example.html')
+st.components.v1.html(net.HTML, height=600)
